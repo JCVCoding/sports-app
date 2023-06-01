2867771670fd67348f3de3d80d495e50ec9de078
Josh <jvcoding9407@gmail.com>
2023-06-01 14:42:11 -0400

    Add NBA logo to NBA Page. Create NFL Page.

--- src/app/nfl/page.tsx
+++ src/app/nfl/page.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image';
-export default function NBA() {
+export default function NFL() {
   return (
     <div className='d-flex flex-column mt-4 align-items-center'>
       <Image
-        src='https://cdn.bleacherreport.net/images/team_logos/328x328/nba.png'
+        src='https://cdn.bleacherreport.net/images/team_logos/328x328/nfl.png'
         alt='NBA logo'
         width={80}
         height={80}
       ></Image>
-      <h2>NBA</h2>
+      <h2>NFL</h2>
     </div>
   );
 }
