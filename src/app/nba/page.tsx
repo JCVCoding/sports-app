67f7b4eb5ed43cde362b784c6d70d1191c2f0774
Josh <jvcoding9407@gmail.com>
2023-06-05 22:06:27 -0400

    Create content card component and place on NBA page

--- src/app/nba/page.tsx
+++ src/app/nba/page.tsx
@@ -1,14 +1,23 @@
 import Image from 'next/image';
+import ContentCard from '@/components/layout_components/contentCard';
+
 export default function NBA() {
   return (
-    <div className='d-flex flex-column mt-4 align-items-center'>
-      <Image
-        src='https://cdn.bleacherreport.net/images/team_logos/328x328/nba.png'
-        alt='NBA logo'
-        width={80}
-        height={80}
-      ></Image>
-      <h2>NBA</h2>
-    </div>
+    <section>
+      <div className='flex flex-col items-center gap-y-3 mt-4'>
+        <Image
+          src='https://cdn.bleacherreport.net/images/team_logos/328x328/nba.png'
+          alt='NBA logo'
+          width={80}
+          height={80}
+        ></Image>
+        <h2 className='text-5xl font-bold text-center mb-4'>NBA</h2>
+      </div>
+      <ol>
+        <li className='my-8 mx-auto'>
+          <ContentCard />
+        </li>
+      </ol>
+    </section>
   );
 }
---
+++ src/components/layout_components/contentCard.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const ContentCard = () => {
+  return (
+    <div>
+      <div>
+        <Link href={'/nba'}>
+          <h3 className='text-2xl font-medium pb-3'>Title</h3>
+        </Link>
+        <p className='text-xl mb-4'>Story Description</p>
+      </div>
+      <div>
+        <Link href={'/nba'}>Image Placeholder</Link>
+      </div>
+      <div className='text-sm text-gray-400 pt-3'>via Source</div>
+    </div>
+  );
+};
+
+export default ContentCard;
