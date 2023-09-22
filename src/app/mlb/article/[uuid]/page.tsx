7f12a81386fa1737082a23521de71491ca677a1e
Josh <jvcoding9407@gmail.com>
2023-09-22 14:02:12 -0400

    Update structure of article pages and add image component

--- src/app/mlb/article/[uuid]/page.tsx
+++ src/app/mlb/article/[uuid]/page.tsx
@@ -1,11 +1,16 @@
+import Image from 'next/image';
 import { getArticleById } from '@/lib/getArticleData';
 
 export default async function Page({ params }: { params: { uuid: string } }) {
   const [data] = await getArticleById(params.uuid, 'MLB_Articles');
   return (
-    <div>
-      <h3 className='text-2xl font-medium pb-3'>{data.title}</h3>
+    <>
+      <h3 className='text-4xl font-medium mb-3'>{data.title}</h3>
+      <div className='text-gray-400 pb-3'>{data.source}</div>
+      <div className='relative mb-3' style={{ height: '800px' }}>
+        <Image src={data.image_url} alt='' fill />
+      </div>
       {data.description}
-    </div>
+    </>
   );
 }
