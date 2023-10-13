101a5c4ce074d81b2329d263da210c0b03b755d8
Josh <jvcoding9407@gmail.com>
2023-10-13 14:35:41 -0400

    Refactor the comment reply dialog component's imports, add state that keeps track of the input value as it changes so that the newly added disabled state to the reply button can be toggled based on if there is an empty string or not in the input.

--- src/components/layout_components/comment/CommentReplyDialog.tsx
+++ src/components/layout_components/comment/CommentReplyDialog.tsx
@@ -1,13 +1,15 @@
+'use client';
+
 import { Avatar, Button, Input } from '@material-tailwind/react';
-import { forwardRef } from 'react';
-import { Ref } from 'react';
+import { forwardRef, Ref, useState } from 'react';
 
 const CommentReplyDialog = (
   { closeDialog }: { closeDialog: () => void },
   ref: Ref<HTMLInputElement>
 ) => {
+  const [inputValue, setInputValue] = useState<string>('');
   return (
-    <div className='flex'>
+    <div className='flex flex-wrap'>
       <Avatar src='/next.svg' size='xs' className='relative top-4' />
       <div className='flex-1 pl-4'>
         <Input
@@ -15,6 +17,7 @@ const CommentReplyDialog = (
           variant='static'
           placeholder='Add a reply...'
           inputRef={ref}
+          onChange={(e) => setInputValue(e.target.value)}
         />
         <div className='flex items-center justify-end gap-x-2 pt-2'>
           <Button
@@ -26,7 +29,9 @@ const CommentReplyDialog = (
             Cancel
           </Button>
           <Button
-            className='rounded-full normal-case text-sm'
+            className='rounded-full normal-case text-sm disabled:bg-gray-300 disabled:text-gray-600'
+            color='blue'
+            disabled={inputValue == ''}
             ripple={false}
             size='sm'
             type='submit'
