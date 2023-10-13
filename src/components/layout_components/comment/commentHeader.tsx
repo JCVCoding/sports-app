b154c6f770999feccf9e5ab1c595b215476a3377
Josh <jvcoding9407@gmail.com>
2023-10-13 14:37:03 -0400

    Add more template code to the comment header component. Add the comment box component to the comment header component's template. Hard coded in the number of comments (make dynamic in the future).

--- src/components/layout_components/comment/commentHeader.tsx
+++ src/components/layout_components/comment/commentHeader.tsx
@@ -1,5 +1,14 @@
+import CommentBox from './commentBox';
+
 const CommentHeader = () => {
-  return <div>This is the comment header</div>;
+  return (
+    <div className='flex flex-col my-8 gap-y-4'>
+      <div>268 Comments</div>
+      <div>
+        <CommentBox />
+      </div>
+    </div>
+  );
 };
 
 export default CommentHeader;
