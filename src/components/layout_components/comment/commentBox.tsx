a08d1f1db689d40fe0c0718afb2373cbcc901152
Josh <jvcoding9407@gmail.com>
2023-10-13 14:28:58 -0400

    Create comment box component

---
+++ src/components/layout_components/comment/commentBox.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { Avatar, Button, Input } from '@material-tailwind/react';
+import { useState } from 'react';
+
+const CommentBox = () => {
+  const [open, setOpen] = useState(false);
+  const [inputValue, setInputValue] = useState<string>('');
+  const openOnFocus = () => {
+    setOpen(true);
+  };
+  const close = () => {
+    setOpen(false);
+  };
+  return (
+    <div className='flex flex-row'>
+      <Avatar src='/next.svg' className='mr-4' />
+      <div className='flex-1'>
+        <Input
+          crossOrigin={undefined}
+          variant='static'
+          placeholder='Add a comment...'
+          onClick={openOnFocus}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        {open ? (
+          <div className='flex items-center justify-end gap-x-2 pt-2'>
+            <Button
+              className='rounded-full normal-case text-sm'
+              size='sm'
+              variant='text'
+              onClick={close}
+            >
+              Cancel
+            </Button>
+            <Button
+              className='rounded-full normal-case text-sm disabled:bg-gray-300 disabled:text-gray-600'
+              color='blue'
+              disabled={inputValue == ''}
+              ripple={false}
+              size='sm'
+              type='submit'
+              variant='filled'
+            >
+              Comment
+            </Button>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default CommentBox;
