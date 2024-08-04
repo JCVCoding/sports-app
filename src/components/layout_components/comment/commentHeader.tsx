870c8c283b8078265021d504e157428f9ad0b0fa
josh <jvcoding9407@gmail.com>
2024-08-03 20:43:45 -0700

    Add total comment count back to commentHeader component

--- src/components/layout_components/comment/commentHeader.tsx
+++ src/components/layout_components/comment/commentHeader.tsx
@@ -1,20 +1,22 @@
+import { useAppSelector } from "@/lib/hooks";
 import CommentBox from "./commentBox";
+import { CommentDataType } from "./commentTypes";
 
 const CommentHeader = () => {
-  // const getRepliesCount = (comments) => {
-  //   let numOfCommentReplies = 0;
-  //   if (comments) {
-  //     comments.data.forEach((comment) => {
-  //       numOfCommentReplies += comment.reply.length;
-  //     });
-  //   }
-  //   return numOfCommentReplies;
-  // };
+  const comments = useAppSelector((state) => state.commentReducer.comments);
+  const getRepliesCount = (comments: CommentDataType[]) => {
+    let numOfCommentReplies = 0;
+    if (comments) {
+      comments.forEach((comment) => {
+        numOfCommentReplies += comment.reply.length;
+      });
+    }
+    return numOfCommentReplies;
+  };
   return (
     <div className="flex flex-col my-8 gap-y-4">
       <div>
-        {/* {comments ? comments.data.length + getRepliesCount(comments) : ""}{" "} */}
-        Comments
+        {comments ? comments.length + getRepliesCount(comments) : ""} Comments
       </div>
       <div>
         <CommentBox />
